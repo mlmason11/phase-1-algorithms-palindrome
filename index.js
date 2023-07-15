9b23b919a07a30e7e53f8722e7de4d4974e14cea
Max <mlmason11@gmail.com>
2023-07-15 14:42:52 -0400

    Updating to include line that polishes all words so they contain only lowercase letters

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 
 function isPalindrome(word) {
   // Write your algorithm here
+  let polishedWord = word.replace(/\W_+|[0-9]/g, '').toLowerCase()
   let drow = ''
-  for (let i = word.length - 1; i >= 0; i--) {
-    drow += word[i]
+  for (let i = polishedWord.length - 1; i >= 0; i--) {
+    drow += polishedWord[i]
   }
-  return (drow === word)
+  return drow === polishedWord
 }
 
 /* 
+
   Add your pseudocode here
-  test for odd or even first
-  if odd
 
-  else
-
+  Same thing as the revers string lab, except we now return the value of
+  whether or not the reversed string is equal to the original string,
+  either true or false
 
 */
 
 /*
+
   Add written explanation of your solution here
+
+  We create a new string that is equal to the reverse of the original string,
+  and test whether or not our two strings are strictly equal
+
 */
 
 // You can run `node index.js` to view these console logs
